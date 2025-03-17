e3163553d191682cd9db699608956a87ef22633a
Ray Sointula <ray.sointula@nokia.com>
2025-03-17 09:23:54 +0200

    abi: add missing documentation
    Some comments were missing, fixed
    
    Signed-off-by: Ray Sointula <ray.sointula@nokia.com>

--- include/odp/api/abi-default/init.h
+++ include/odp/api/abi-default/init.h
@@ -17,14 +17,22 @@ extern "C" {
 
 #include <odp/api/std_types.h>
 
-typedef uint64_t odp_instance_t;
-
 /**
  * @internal platform specific data
  */
 typedef struct odp_platform_init_t {
 	char dummy; /**< @internal Dummy */
 } odp_platform_init_t;
+
+/** @addtogroup odp_initialization
+ *  @{
+ */
+
+typedef uint64_t odp_instance_t;
+
+/**
+ * @}
+ */
 
 #ifdef __cplusplus
 }
